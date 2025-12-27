355c3716adb442ef7e32f3b5becaea6ade8d583f
lebrancconvas <lebrancconvas@gmail.com>
2025-12-28 02:21:59 +0700

    :sparkles: feat: export class page.

--- libs/page.ts
+++ libs/page.ts
@@ -2,10 +2,16 @@ import { chromium } from "playwright";
 import { CONFIG } from "../config/config";
 
 class Page {
-  static async create(): Promise<Page> {
-    const browser = await chromium.launch({ headless: CONFIG.browser.isHeadless });
-    const context = await browser.newContext({ locale: CONFIG.browser.locale });
-    const page = await context.newPage();
-    return page;
-  }
-};
+    static async create(): Promise<Page> {
+        const browser = await chromium.launch({
+            headless: CONFIG.browser.isHeadless,
+        });
+        const context = await browser.newContext({
+            locale: CONFIG.browser.locale,
+        });
+        const page = await context.newPage();
+        return page;
+    }
+}
+
+export default Page;
